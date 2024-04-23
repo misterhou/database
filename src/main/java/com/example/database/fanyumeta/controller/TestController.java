54493c106504d2288c51341fea152483ee968cbe
misterhou <misterhou@aliyun.com>
2024-04-23 17:05:49 +0800

    1、新增 8017 服务模拟功能 2、新增更新指令缓存功能

---
+++ src/main/java/com/example/database/fanyumeta/controller/TestController.java
@@ -0,0 +1,47 @@
+package com.example.database.fanyumeta.controller;
+
+import com.example.database.fanyumeta.client.HardwareControlClient;
+import com.example.database.fanyumeta.utils.HardwareControlCommandUtil;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+
+@RestController
+@RequestMapping("/test")
+public class TestController {
+
+    @Resource
+    private HardwareControlClient hardwareControlClient;
+
+    @Value("${fan-yu.hardware-control.request-command-config-file}")
+    private String requestCommandConfigFile;
+
+    @Value("${fan-yu.hardware-control.receive-command-config-file}")
+    private String receiveCommandConfigFile;
+
+    @GetMapping("/hardware/send/message")
+    public String sendMessage(String message) {
+        this.hardwareControlClient.sendMessage(message);
+        return "success";
+    }
+
+    @GetMapping("/hardware/send/command")
+    public String sendCommon(String message) {
+        return this.hardwareControlClient.sendCommand(message);
+    }
+
+    @PostMapping("/imitate/8017")
+    public String imitate8017(String message) {
+        return "{\"id\": \"39\", \"question\": \"\\u5927\\u5c4f\\u524d\\u897f\\u4fa7\\u7a7a\\u8c03\\u6253\\u5f00\", \"cate\": \"\\u628a\\u5927\\u5c4f\\u524d\\u897f\\u4fa7/\\u4e1c\\u4fa7\\u7a7a\\u8c03\\u6e29\\u5ea6\\u8c03\\u523023\\u5ea6\", \"directiveType\": \"\\u8fd0\\u884c\\u6307\\u4ee4\", \"directive\": \"\\u65e0\"}";
+    }
+
+    @GetMapping("/update/command/cache")
+    public String updateCommandCache() {
+        HardwareControlCommandUtil.initCache(
+                this.requestCommandConfigFile,
+                this.receiveCommandConfigFile);
+        return "success";
+    }
+
+}
