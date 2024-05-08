a2fd3d41aea2f8fe15b759ab792566ce1553f5bb
misterhou <misterhou@aliyun.com>
2024-05-08 15:18:18 +0800

    调整代码结构

--- src/main/java/com/example/database/DatabaseApplication.java
+++ src/main/java/com/example/database/DatabaseApplication.java
@@ -1,12 +1,17 @@
 package com.example.database;
 
+import com.alibaba.fastjson.JSONObject;
 import com.example.database.fanyumeta.utils.HardwareControlCommandUtil;
 import com.example.database.fanyumeta.utils.PicDataUtil;
+import org.apache.ibatis.ognl.Ognl;
+import org.apache.ibatis.ognl.OgnlException;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.core.env.Environment;
+
+import java.util.Map;
 
 @MapperScan("com.example.database.mapper")
 @SpringBootApplication
@@ -20,33 +25,33 @@ public class DatabaseApplication {
                 environment.getProperty("fan-yu.hardware-control.receive-command-config-file"));
         PicDataUtil.initPicData(environment.getProperty("fan-yu.tell-how.pic-config-file"));
 //        testOgnl();
+//        generateCacheFile();
     }
 
-//    public static void main(String[] args) throws Exception {
-//        HardwareControlCommandUtil.generateRequestCommandCacheFile("src/main/resources/中控指令集.xlsx",
-//                "RequestCommandCache");
-//        HardwareControlCommandUtil.generateReceiveCommandCacheFile("src/main/resources/中控指令集.xlsx",
-//                "ReceiveCommandCache");
-//        HardwareControlCommandUtil.generateRequestReceiveCommandCacheFile("src/main/resources/realtime-public-100092-org.xlsx",
-//                "RequestReceiveCommandCache");
-//        PicDataUtil.generatePicDataCacheFile("src/main/resources/开图清单.xlsx",
-//                "PicDataCache");
-//        Pattern pattern = Pattern.compile("(28|二十八)度参观台东侧空调|参观台东侧空调(28|二十八)度");
-//        Pattern pattern = Pattern.compile("(18|(十八))度参观台东侧空调|参观台东侧空调(18|十八)度");
-//        if (pattern.matcher("二十八度参观台东侧空调").matches()) {
-//            System.out.println("ok");
-//        }
-//    }
+    private static void generateCacheFile() {
+        try {
+            HardwareControlCommandUtil.generateRequestCommandCacheFile("src/main/resources/中控指令集.xlsx",
+                    "RequestCommandCache");
+            HardwareControlCommandUtil.generateReceiveCommandCacheFile("src/main/resources/中控指令集.xlsx",
+                    "ReceiveCommandCache");
+            HardwareControlCommandUtil.generateRequestReceiveCommandCacheFile("src/main/resources/中控指令集.xlsx",
+                    "RequestReceiveCommandCache");
+            PicDataUtil.generatePicDataCacheFile("src/main/resources/开图清单.xlsx",
+                    "PicDataCache");
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
 
-//    private static void testOgnl() {
-//        JSONObject responseData = JSONObject.parseObject("{\"tabData\":\"无\",\"jpgPath\":\"无\",\"id\":\"无\",\"results\":{\"answer\":\"回答内容\",\"docs\":[]}}");
-//        Map defaultContext = Ognl.createDefaultContext(responseData);
-//        try {
-//            Object value = Ognl.getValue("results1.answer1", responseData);
-//            System.out.println(value);
-//        } catch (OgnlException e) {
-//            throw new RuntimeException(e);
-//        }
-//    }
+    private static void testOgnl() {
+        JSONObject responseData = JSONObject.parseObject("{\"tabData\":\"无\",\"jpgPath\":\"无\",\"id\":\"无\",\"results\":{\"answer\":\"回答内容\",\"docs\":[]}}");
+        Map defaultContext = Ognl.createDefaultContext(responseData);
+        try {
+            Object value = Ognl.getValue("results1.answer1", responseData);
+            System.out.println(value);
+        } catch (OgnlException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
 }
